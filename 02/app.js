98af049e0154f74049de05d13e3bd1c593e84697
Michal Robert Adamczyk <michal.robert.adamczyk@gmail.com>
2020-03-25 13:41:56 +0100

    first addeventlistener, first function

---
+++ 02/app.js
@@ -0,0 +1,4 @@
+const btnCalculate = document.querySelector('.cart__btn-calculate');
+// console.log(btnCalculate);
+const test = (a) => {console.log('funkcja test działa')};
+btnCalculate.addEventListener('click', test); 
