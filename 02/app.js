032149fce0852c45777291faaf20f962963692c3
Michal Robert Adamczyk <michal.robert.adamczyk@gmail.com>
2020-03-25 18:32:26 +0100

    correct sum done

--- 02/app.js
+++ 02/app.js
@@ -3,14 +3,22 @@ const btnCalculate = document.querySelector('.cart__btn-calculate');
 // const InputList = document.getElementsByClassName('cart__item');
 // const arr = [...InputList];
 
+const ArrayToSum = []
+
 
 const test = (a) =>{
     
     const list= document.getElementsByClassName("cart__item");
     [].forEach.call(list, itteration = el => {
-        console.log(el.querySelector('input').dataset.price);
-
+        // console.log(el.querySelector('input').dataset.price*el.querySelector('input').value);
+        ArrayToSum.push(el.querySelector('input').dataset.price*el.querySelector('input').value);
     });
+    console.log(ArrayToSum);
+    const sum = ArrayToSum.reduce((prev, n) => {
+       return prev + n;
+    });
+    console.log(sum);
+
 } 
 
 
