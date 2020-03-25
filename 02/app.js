8c0f2eedd5b193759408fad855b1c07edcdea1c8
Michal Robert Adamczyk <michal.robert.adamczyk@gmail.com>
2020-03-25 17:11:58 +0100

    nie potrafię zrobić itteracji

--- 02/app.js
+++ 02/app.js
@@ -1,4 +1,18 @@
 const btnCalculate = document.querySelector('.cart__btn-calculate');
 // console.log(btnCalculate);
-const test = (a) => {console.log('funkcja test działa')};
+const InputList = document.getElementsByClassName('cart__item');
+const arr = [...InputList];
+
+
+const test = (a) =>{
+    console.log('funkcja test działa', arr);
+    for each (el in arr) {
+        console.log(index);
+      }
+} 
+
+
 btnCalculate.addEventListener('click', test); 
+
+// przypisać zmienne do cen, ilości produktów i .cart__total-price
+
