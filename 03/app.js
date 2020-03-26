b3faa8a5a6f62d48e9ee0c07ee1271b70927423a
Michal Robert Adamczyk <michal.robert.adamczyk@gmail.com>
2020-03-26 13:32:36 +0100

    done

--- 03/app.js
+++ 03/app.js
@@ -1,2 +1,24 @@
 import {files} from './data.js';
 
+const ArrayToSum = []
+
+
+const unitsChanger = (ProtoUnit, sizeOfUnit) => {
+    let converter=1;
+    if(ProtoUnit==='KB')converter=1024;
+    if(ProtoUnit==='MB')converter=1024*1024;
+    if(ProtoUnit==='GB')converter=1024*1024*1024;
+    let FinalUnit = sizeOfUnit*converter;
+    ArrayToSum.push(FinalUnit); 
+}
+
+
+files.forEach(element => {
+    unitsChanger(element.size.unit, element.size.length);
+
+});
+
+const sum = ArrayToSum.reduce((prev, n) => {
+    return prev + n;
+ });
+ console.log(sum);
