a9f35382ed9bd507a20826ea5ec3e7a9478a151c
Michal Robert Adamczyk <michal.robert.adamczyk@gmail.com>
2020-03-30 12:22:39 +0200

    zrobiłem ale czy dobrze?

--- 04/app.js
+++ 04/app.js
@@ -1,19 +1,12 @@
 import User from './User.js';
 
-const userData = { login: 'user@devmentor.pl', password: '123456' }
-const adminData = { login: 'admin@devmentor.pl', password: '1234567890' }
-
-const user = new User( userData );
-user.register();
-
-class Admin extends User{
-    constructor( {login = '', password = ''} ){
+class Admin extends User {
+    constructor( {login = '', password = ''} ) {
         super( {login, password} )
+        this.login = login;
+        this.password = password;
     }
-    
-    // super.register()
-    // super.isValid()
-    // super.isLoginCorrect() 
+
     isPasswordCorrect() {
         if(this.password.length >= 10) {
             return true;
@@ -23,9 +16,16 @@ class Admin extends User{
     }
 }
 
+const userData = { login: 'user@devmentor.pl', password: '123456' }
+const adminData = { login: 'admin@devmentor.pl', password: '1234567890' }
+
+const user = new User( userData );
+user.register();
+
+
+
 const admin1 = new Admin( userData );
 admin1.register(); // error
 
 const admin2 = new Admin( adminData );
 admin2.register(); // done
-```
